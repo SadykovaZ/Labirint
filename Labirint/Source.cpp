2579e23e589fc7d5549aec7b9c3bf453757f7dbe
Жанара <@DESKTOP-O9KC10I>
2018-08-24 15:12:22 +0600

    final

--- Labirint/Source.cpp
+++ Labirint/Source.cpp
@@ -1,59 +1,73 @@
-#include<iostream>
-#include<time.h>
+#include <iostream>
+
 using namespace std;
-const int n = 4;
-const int m = 5;
-int arr[n][m] = { { 1,1,1,0,0 },{ 0,0,1,1,1 },{ 0,0,0,1,1 },{ 1,1,0,0,1} };
-bool fs[n][m] = { 0 };
-int exitx, exity, flag;
-int exit_lab(int i, int j)
+
+const int n = 4; //строки
+const int m = 4; // столбцы
+
+int a[n][m] = { { 1, 1, 0, 0 }, { 0, 1, 1,0 }, { 0, 0, 1,1 }, {0, 0, 1,1} };
+
+bool f[n][m] = { 0 };
+
+void lab(int i, int j)
 {
-	if (arr[i][j]==0)
-	{
-		return 0;
-	}
-	if (arr[exitx][exity] == 1)
-	{
-		flag = 1;
-		return 0;
-	}
 
-
-	if ((j >= 0 && j < m) && (i >= 0 && i < n))
+	if ((j >= 0 && j < n) && (i >= 0 && i < m))
 	{
-		
-		fs[i][j] = true;
-		if (arr[i][j - 1] != 0 && fs[i][j - 1] == false) exit_lab(i, j - 1);
-		if (arr[i][j + 1] != 0 && fs[i][j + 1] == false) exit_lab(i, j + 1);
-		if (arr[i - 1][j] != 0 && fs[i - 1][j] == false) exit_lab(i - 1, j);
-		if (arr[i + 1][j] != 0 && fs[i + 1][j] == false) exit_lab(i + 1, j);
-		if (arr[i - 1][j - 1] != 0 && fs[i - 1][j - 1] == false) exit_lab(i - 1, j - 1);
-		if (arr[i - 1][j + 1] != 0 && fs[i - 1][j + 1] == false) exit_lab(i - 1, j + 1);
-		if (arr[i + 1][j - 1] != 0 && fs[i + 1][j - 1] == false) exit_lab(i + 1, j - 1);
-		if (arr[i + 1][j + 1] != 0 && fs[i + 1][j + 1] == false) exit_lab(i + 1, j + 1);
-		
+		if (a[m - 1][n - 1] == 1 && f[m - 1][n - 1] == true)
+		{
+			return;
+		}
+		f[i][j] = true;
+
+		cout << i << " " << j << endl;
+
+		if (a[i][j - 1] == 1 && f[i][j - 1] == false)
+
+			lab(i, j - 1);
+
+		if (a[i][j + 1] == 1 && f[i][j + 1] == false)
+
+			lab(i, j + 1);
+
+		if (a[i - 1][j] == 1 && f[i - 1][j] == false)
+
+			lab(i - 1, j);
+
+		if (a[i + 1][j] == 1 && f[i + 1][j] == false)
+
+			lab(i + 1, j);
 	}
+}
+
+void lab1(int i, int j) {
+
+	lab(i, j);
+
+	if (f[m - 1][n - 1] == true)
+		cout << "Выход есть. " << endl;
+
+	else
+		cout << "Выхода нет." << endl;
 
 }
 
 int main()
+
 {
 	setlocale(LC_ALL, "Rus");
-	srand(time(NULL));
-
-	cout << "На основе задачи с инфициированными клетками решите задачу лабиринта" << endl;
-	
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++)
 		{
-			cout << arr[i][j] << " ";
+			cout << a[i][j] << " ";
 		}
 		cout << endl;
 	}
-	exit_lab(1, 2);
-
+	cout << endl;
+	lab1(0, 0);
 
 	system("pause");
 	return 0;
+
 }
